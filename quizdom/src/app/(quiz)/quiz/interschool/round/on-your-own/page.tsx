26922e5f2e0b541b327181b9e17e222e6cfd7da0
XenoXtreme <masterbuilders1971@gmail.com>
2025-07-20 14:58:01 +0000

    Fixed small error.

--- quizdom/src/app/(quiz)/quiz/interschool/round/on-your-own/page.tsx
+++ quizdom/src/app/(quiz)/quiz/interschool/round/on-your-own/page.tsx
@@ -1,14 +1,17 @@
-import { Metadata } from 'next';
-import Panel from '@/components/website/quiz/panel/question-panel/oyo';
+import { Metadata } from "next";
+import Panel from "@/components/website/quiz/panel/question-panel/oyo";
 
 // METADATA
 export const metadata: Metadata = {
-    title: "On Your Own - Interschool",
-    description: "Annual quiz competition of Jalpaiguri Zilla School",
-  };
+  title: "On Your Own - Interschool",
+  description:
+    "Annual quiz competition of Jalpaiguri Zilla School and other schools in Jalpaiguri district",
+};
 
 export default function Page() {
   return (
-    <div><Panel category="interschool" /></div>
-  )
+    <div>
+      <Panel />
+    </div>
+  );
 }
